587559af2968282eca5fb3e2a2d0dcffabac4fe4
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-15 22:44:22 +0900

    Add Example01 class

--- src/main/java/com/example/jpa/JpaApplication.java
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -1,18 +1,16 @@
 package com.example.jpa;
 
-import jakarta.persistence.*;
+import com.example.jpa.examples.Example01;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.text.MessageFormat;
-import java.util.List;
-
 @SpringBootApplication
 public class JpaApplication {
-	@PersistenceUnit
-	EntityManagerFactory emf;
+	@Autowired
+	Example01 example01;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
@@ -21,49 +19,7 @@ public class JpaApplication {
 	@Bean
 	public CommandLineRunner run() {
 		return args -> {
-			EntityManager em = emf.createEntityManager();
-			EntityTransaction tx = em.getTransaction();
-
-			try {
-				tx.begin();
-				logic(em);
-				tx.commit();
-			} catch (Exception e) {
-				tx.rollback();
-			} finally {
-				em.close();
-			}
-
-			emf.close();
+			example01.run();
 		};
-	}
-
-	void logic(EntityManager em) {
-		String id = "id1";
-		Agent agent = new Agent();
-		agent.setId(id);
-		agent.setUsername("윤영우");
-		agent.setAge(29);
-
-		// 등록
-		em.persist(agent);
-
-		// 수정(JPA는 어떤 엔티티가 변경되었는지 추적하는 기능을 갖추고 있다)
-		agent.setAge(30);
-
-		// 한 건 조회
-		Agent findAgent = em.find(Agent.class, id);
-		System.out.println(MessageFormat.format("""
-				아이디 : {0}
-				이름 : {1}
-				나이 : {2}""", findAgent.getId(), findAgent.getUsername(), findAgent.getAge()));
-
-		// 목록 조회
-		List<Agent> agentList = em.createQuery("""
-				select a from agent a""", Agent.class).getResultList();
-		System.out.println(MessageFormat.format("상담원 수 : {0}", agentList.size()));
-
-		// 삭제
-		em.remove(agent);
 	}
 }
---
+++ src/main/java/com/example/jpa/examples/Example01.java
@@ -0,0 +1,63 @@
+package com.example.jpa.examples;
+
+import com.example.jpa.Agent;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceUnit;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+@Component
+public class Example01 {
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    public void run() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private void logic(EntityManager em) {
+        String id = "id1";
+        Agent agent = new Agent();
+        agent.setId(id);
+        agent.setUsername("윤영우");
+        agent.setAge(29);
+
+        // 등록
+        em.persist(agent);
+
+        // 수정(JPA는 어떤 엔티티가 변경되었는지 추적하는 기능을 갖추고 있다)
+        agent.setAge(30);
+
+        // 한 건 조회
+        Agent findAgent = em.find(Agent.class, id);
+        System.out.println(MessageFormat.format("""
+				아이디 : {0}
+				이름 : {1}
+				나이 : {2}""", findAgent.getId(), findAgent.getUsername(), findAgent.getAge()));
+
+        // 목록 조회
+        List<Agent> agentList = em.createQuery("""
+				select a from agent a""", Agent.class).getResultList();
+        System.out.println(MessageFormat.format("상담원 수 : {0}", agentList.size()));
+
+        // 삭제
+        em.remove(agent);
+    }
+}
